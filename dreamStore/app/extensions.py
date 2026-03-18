d544fc8fbcc594acfc0e446f5718ba9ca9f4e369
Pre-spirit <106741153+Pre-spirit@users.noreply.github.com>
2026-03-18 20:01:10 +0800

    Delete dreamStore directory

--- dreamStore/app/extensions.py
+++
@@ -1,8 +0,0 @@
-# 单独管理扩展对象，避免循环导入
-from flask_sqlalchemy import SQLAlchemy
-from flask_migrate import Migrate
-
-db = SQLAlchemy()
-migrate = Migrate()
-
-
--- dreamStore/app/utils/response.py
+++
@@ -1,6 +0,0 @@
-def make_response(code=200, message='success', data=None):
-    return {
-        'code': code,
-        'message': message,
-        'data': data or {}
-    }
